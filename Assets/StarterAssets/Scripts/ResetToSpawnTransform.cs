29716e822082e2d08a511609f9806905066fb791
maciek1323 <maciek.janiak.j13@gmail.com>
2023-06-14 16:42:07 +0200

    środa 14 06 resetowania działa

--- Assets/StarterAssets/Scripts/ResetToSpawnTransform.cs
+++ Assets/StarterAssets/Scripts/ResetToSpawnTransform.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ResetToSpawnTransform : MonoBehaviour
-{ public Transform transformToreset;
+{
+    public Transform transformToreset;
 
     public Vector3 StartLocation;
 
-        public Vector3 StartRotation;
+    public Vector3 StartRotationAngles;
+
+    public Quaternion StartRotation;
     // Start is called before the first frame update
     void Start()
     {
+        if (transformToreset== null)
+        {
+
+            transformToreset = transform;
+
+        }
+
+
+
         StartLocation = transformToreset.position;
+        StartRotation = transformToreset.rotation;
+        StartRotationAngles = StartRotation.eulerAngles;  
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("2137");
+            transformToreset.position = StartLocation;
+            StartRotation.eulerAngles = StartRotationAngles;
+            transformToreset.rotation = StartRotation;
+        }
     }
+
 }
